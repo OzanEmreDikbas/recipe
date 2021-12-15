e34621ccbef7212fc75a8dc3676fae3e4166cfd1
Ozan Emre Dikbaş <87207946+OzanEmreDikbas@users.noreply.github.com>
2021-12-16 00:32:31 +0300

    4. commit.

--- src/pages/details/Details.js
+++ src/pages/details/Details.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const About = () => {
+const Details = () => {
     return (
         <div>
-            <h1>About</h1>
+            <h1>Details</h1>
         </div>
     )
 }
 
-export default About
+export default Details
